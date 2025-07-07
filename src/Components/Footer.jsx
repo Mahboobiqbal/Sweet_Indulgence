5ab608108b633743b92aca5ba2bfd14dcc937beb
syedsyab <syabblogger@gmail.com>
2025-07-08 01:56:44 +0500

    fix: Update application name and footer contact information to reflect new branding for Bake House

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -71,7 +71,7 @@ const Footer = () => {
               </a>
             </li>
             <li className="flex items-center">
-              <FaWhatsapp className="mr-2" /> +92 345 123456
+              <FaWhatsapp className="mr-2" /> +92 345 1234567
             </li>
             <li>Mingora, Swat, Pakistan</li>
           </ul>
@@ -112,7 +112,7 @@ const Footer = () => {
         {/* Copyright */}
         <div className="col-span-1 md:col-span-4 text-center mt-8 pt-8 border-t border-[#8c5f53]">
           <p className="text-[#8c5f53]">
-            © {new Date().getFullYear()} © 2025 Bake House. All Rights Reserved.
+            © {new Date().getFullYear()} Bake House. All Rights Reserved.
           </p>
         </div>
       </div>
